ab0b6c08d0ab11e611a71eedf9d59b3d3ea06551
amit das <mramitdas99@gmail.com>
2023-11-11 01:35:50 +0530

    add user_uuid in user_schema and the documentation along with examples
    Closes #14

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,4 @@
-import re
-
-from pydantic import BaseModel, EmailStr, conint, constr, validator
+from pydantic import BaseModel, EmailStr, constr
 
 from .utils import TimestampMixin, generate_password
 
@@ -12,9 +10,20 @@ class BaseUser(BaseModel):
     Attributes:
         full_name (str): The full name of the user, constrained to 50 characters with leading/trailing whitespaces stripped.
         email (EmailStr, optional): The email address of the user, if provided.
+        github_profile (str, optional): The GitHub profile of the user, if provided.
 
     Config:
         from_attributes (bool): Indicates whether attribute values should be populated from the corresponding class attributes when creating an instance. Defaults to True.
+
+    Examples:
+        >>> user_data = {"full_name": "John Doe", "email": "john@example.com", "github_profile": "johndoe"}
+        >>> user = BaseUser(**user_data)
+        >>> user.full_name
+        'John Doe'
+        >>> user.email
+        'john@example.com'
+        >>> user.github_profile
+        'johndoe'
     """
 
     full_name: constr(strip_whitespace=True, max_length=50) | None = None
@@ -37,14 +46,8 @@ class UserIn(TimestampMixin, BaseUser):
     Represents a user input model with additional fields and validation.
 
     Attributes:
-        password (str): The password associated with the user. Defaults to randomly generated password.
-        is_active (bool): Indicates whether the user account is active. Defaults to True.
-        is_staff (bool): Indicates whether the user has staff privileges. Defaults to False.
-        is_admin (bool): Indicates whether the user has admin privileges. Defaults to False.
-        is_superuser (bool): Indicates whether the user has superuser privileges. Defaults to False.
-
-    Validators:
-        check_aadhar_no (classmethod): A validator method that checks the Aadhar number (if provided) for its format. Raises a ValueError if the format is invalid.
+        user_uuid (int, optional): The unique identifier for the user, if provided.
+        password (str): The password associated with the user. Defaults to a randomly generated password.
 
     Config:
         from_attributes (bool): Indicates whether attribute values should be populated from the corresponding class attributes when creating an instance. Defaults to True.
@@ -56,8 +59,23 @@ class UserIn(TimestampMixin, BaseUser):
     Note:
         The `check_aadhar_no` validator is applied to the `aadhar_no` field if a value is provided and ensures it is exactly 12 digits long.
 
+    Examples:
+        >>> user_data = {"full_name": "John Doe", "email": "john@example.com", "github_profile": "johndoe", "password": "secretpass"}
+        >>> user = UserIn(**user_data)
+        >>> user.full_name
+        'John Doe'
+        >>> user.email
+        'john@example.com'
+        >>> user.github_profile
+        'johndoe'
+        >>> user.password
+        'secretpass'
+        >>> user.created_at
+        datetime.datetime(...)
+
     """
 
+    user_uuid: int | None = None
     password: str = generate_password()
 
     class Config:
@@ -78,13 +96,26 @@ class UserOut(BaseUser):
     This class inherits attributes and behavior from the `BaseUser` class and is intended to be used for representing user data in output or response objects.
 
     Attributes:
-        - user_uuid (int): The unique identifier for the user.
+        user_uuid (int): The unique identifier for the user.
 
     Inherits from:
-        - BaseUser: The base user model with common attributes.
+        BaseUser: The base user model with common attributes.
 
     Note:
         This class does not introduce additional attributes or behavior beyond what is defined in the `BaseUser` class. It serves as a specialized version of `BaseUser` specifically designed for representing user data in response objects.
+
+    Examples:
+        >>> user_data = {"full_name": "John Doe", "email": "john@example.com", "github_profile": "johndoe", "user_uuid": 123}
+        >>> user = UserOut(**user_data)
+        >>> user.full_name
+        'John Doe'
+        >>> user.email
+        'john@example.com'
+        >>> user.github_profile
+        'johndoe'
+        >>> user.user_uuid
+        123
+
     """
 
     class Config:
