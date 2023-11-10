08c30c50bc8e009c9ca22fab2d4c3c555f7983bd
amit das <mramitdas99@gmail.com>
2023-11-10 13:49:54 +0530

    FEAT: create a post api for user profile registration
    Closes #11

--- app/api/V1/endpoints/user.py
+++ app/api/V1/endpoints/user.py
@@ -1,18 +1,48 @@
-from flask import Blueprint, request
+from flask import Blueprint, abort, request
+from werkzeug.exceptions import (BadRequest, InternalServerError,
+                                 MethodNotAllowed)
 
-user = Blueprint('user', __name__)
+from app.models.user import User as UserModel
+from app.schemas.user import UserOut
 
-
-@user.route('profile/', methods=['GET', 'POST'])
-def get_user_profile():
-    if request.method == 'GET':
-        return "This is a GET request for the user profile."
-    elif request.method == 'POST':
-        return "This is a POST request for the user profile."
-    else:
-        return "Unsupported request method."
+user = Blueprint("user", __name__)
 
 
-@user.route('profiles/', methods=['GET'])
+@user.route("profile/", methods=["POST"])
+def save_user_profile():
+    if request.method == "POST":
+        try:
+            user_data = UserOut(**request.json)
+        except ValueError as e:
+            raise BadRequest(f"Validation error: {e}")
+
+        user_instance = UserModel()
+
+        try:
+            user_dict = user_data.model_dump(exclude_unset=False)
+        except ValueError as e:
+            raise BadRequest(f"Invalid profile data: {e}")
+
+        try:
+            response = user_instance.save(data=user_dict)
+        except Exception as e:
+            raise InternalServerError(f"Failed to register user: {e}")
+
+        if response.acknowledged:
+            return {
+                "status": "success",
+                "message": "Profile registration successful",
+                "data": {
+                    "full_name": user_dict.get("full_name"),
+                    "github_profile": user_dict.get("github_profile"),
+                },
+            }
+        return {"status": "failure", "message": "Profile registration failed"}
+
+    # Handle GET request if needed
+    abort(MethodNotAllowed.code, description="Unsupported request method")
+
+
+@user.route("profiles/", methods=["GET"])
 def get_user_profiles():
     return "profiles"
