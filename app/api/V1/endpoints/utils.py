86a4bf458cb59ed19c772d6985a4c696de0e7eda
amit <mramitdas99@gmail.com>
2023-11-15 02:27:17 +0530

    FEAT:  write an function to take github readme screenshot
    Closes #57

--- app/api/V1/endpoints/utils.py
+++ app/api/V1/endpoints/utils.py
@@ -1,6 +1,5 @@
-import json
-
 import requests
+from pyppeteer import launch
 
 
 def fetch_user_info(username: str):
@@ -39,3 +38,50 @@ def fetch_user_info(username: str):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching user information: {e}")
         return None, {}
+
+
+async def main(github_username: str):
+    """
+    Takes a screenshot of a GitHub user's profile page in dark mode.
+
+    This function launches a headless browser using Pyppeteer, navigates to the GitHub profile page
+    of the specified user, and takes a screenshot with dark mode enabled. The dark mode is achieved
+    by injecting JavaScript code to set the 'data-color-mode' attribute to 'dark'.
+
+    Parameters:
+        github_username (str): The GitHub username of the user whose profile page to capture.
+
+    Returns:
+        None
+
+    Example:
+        >>> asyncio.get_event_loop().run_until_complete(main('mramitdas'))
+
+    Note:
+        Ensure that you have Pyppeteer installed (`pip install pyppeteer`) and have the necessary
+        dependencies (such as Chromium) available in your environment.
+    """
+    browser = await launch()
+    page = await browser.newPage()
+
+    # Emulate desktop environment
+    await page.emulate(
+        {
+            "viewport": {"width": 1920, "height": 1080},
+            "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        }
+    )
+
+    await page.goto(f"https://github.com/{github_username}")
+
+    # Inject JavaScript code to enable dark mode
+    await page.evaluate(
+        """
+        document.documentElement.setAttribute('data-color-mode', 'dark');
+    """
+    )
+
+    await page.screenshot(
+        {"path": f"{github_username}_dark_mode.png", "fullPage": "true"}
+    )
+    await browser.close()
